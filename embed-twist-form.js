7eb0ab90fc5f06d8563a00a2b1b13a15c048baeb
twistcardtwistcard <007strategies@gmail.com>
2025-06-17 16:11:44 -0400

    Delete embed-twist-form.js

--- embed-twist-form.js
+++
@@ -1,5 +0,0 @@
-fetch('https://form-backend-p44w.onrender.com/twist-form.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('twist-form-container').innerHTML = html;
-  });
